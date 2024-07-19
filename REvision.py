5a7142725f43c54a1a6edaf2cdf1e295d3bf4598
codedreammer <nkanal38@gmail.com>
2024-07-19 14:56:46 +0530

    add the file

--- REvision.py
+++ REvision.py
@@ -4,7 +4,19 @@ glass_of_water=3
 
 print("I drank", glass_of_water, "glasses of water today.")
 
+
 glass_of_water=3
 
 glass_of_waters=glass_of_water+2
 print(glass_of_waters)
+
+#Assign a float with 2 decimals to the variable below. If you don't wan't to search the value you can check out Hint 1.
+
+global_mean_sea_level_2018=21
+
+#Type your code here.
+global_mean_sea_level_2018= 21.36
+
+
+print(global_mean_sea_level_2018)
+
